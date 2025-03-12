1822c3cc91e895d50cb74858068ff4b28c89aac0
AndDev <23031429@itcelaya.edu.mx>
2025-03-11 19:47:38 -0600

    Enhance Header component with improved styling and icon integration

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,41 +1,51 @@
+import { FaHome, FaInfoCircle, FaUsers } from "react-icons/fa";
 import { NavLink } from "react-router";
 
 export const Header = () => {
   return (
     <header className="bg-neutral-900 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Mi aplicación</h1>
+      <div className="container mx-auto flex justify-between items-center flex-wrap">
+        <h1 className="text-xl font-bold">
+          <span className="-tracking-wide">Mi:.</span>
+          <span className="font-light tracking-wider text-sm">reactpro</span>
+        </h1>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-4 items-center justify-center">
             <li>
-              <a href="#" className="hover:underline">
-                <NavLink
-                  to="/"
-                  className={({ isActive }) => (isActive ? "underline" : "")}
-                >
-                  Inicio
-                </NavLink>
-              </a>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "font-bold transition-all py-3 px-2 flex items-center gap-2"
+                    : "hover:text-neutral-100 transition-all py-3 px-2 flex items-center gap-2 text-neutral-100/50"
+                }
+              >
+                <FaHome /> <span>Inicio</span>
+              </NavLink>
             </li>
             <li>
-              <a href="#" className="hover:underline">
-                <NavLink
-                  to="/about"
-                  className={({ isActive }) => (isActive ? "underline" : "")}
-                >
-                  Acerca de
-                </NavLink>
-              </a>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive
+                    ? "font-bold transition-all py-3 px-2 flex items-center gap-2"
+                    : "hover:text-neutral-100 transition-all py-3 px-2 flex items-center gap-2 text-neutral-100/50"
+                }
+              >
+                <FaInfoCircle /> <span>Acerca de</span>
+              </NavLink>
             </li>
             <li>
-              <a href="#" className="hover:underline">
-                <NavLink
-                  to="/users"
-                  className={({ isActive }) => (isActive ? "underline" : "")}
-                >
-                  Usuarios
-                </NavLink>
-              </a>
+              <NavLink
+                to="/users"
+                className={({ isActive }) =>
+                  isActive
+                    ? "font-bold transition-all py-3 px-2 flex items-center gap-2"
+                    : "hover:text-neutral-100 transition-all py-3 px-2 flex items-center gap-2 text-neutral-100/50"
+                }
+              >
+                <FaUsers /> <span>Usuarios</span>
+              </NavLink>
             </li>
           </ul>
         </nav>

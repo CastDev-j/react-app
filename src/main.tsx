d4063583e64be4494f0f27b670e22f0ca36be91e
AndDev <23031429@itcelaya.edu.mx>
2025-03-12 22:38:45 -0600

    Add @vercel/analytics dependency and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Navigation } from "./routes/Navigation.tsx";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Navigation />
+    <Analytics />
   </StrictMode>
 );
